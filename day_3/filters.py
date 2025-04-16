ab13936179e77c533f5332ab1227e6f472940cd4
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 22:44:32 +0530

    Committed the files of filters, watermark removal

--- day_3/filters.py
+++ day_3/filters.py
@@ -1,1 +1,14 @@
+import cv2
 
+# Load the image
+image = cv2.imread(r"C:\Users\KushDoshi\Downloads\Kush Doshi.jpg")
+
+resize_factor = 0.2
+resized_image = cv2.resize(image, (0, 0), fx=resize_factor, fy=resize_factor)
+
+blurred_image = cv2.GaussianBlur(resized_image, (15, 15), 0)
+
+cv2.imshow('Original Image', resized_image)
+cv2.imshow('Blurred Image', blurred_image)
+
+cv2.waitKey(0)
---
+++ day_3/watermark.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+image_path = r"C:\Users\KushDoshi\Downloads\64908027.jpg"
+image = cv2.imread(image_path)
+
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+lower_blue = np.array([85, 20, 155])
+upper_blue = np.array([105, 205, 255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+kernel = np.ones((3, 3), np.uint8)
+mask = cv2.dilate(mask, kernel, iterations=3)
+
+inpainted = cv2.inpaint(image, mask, inpaintRadius=3, flags=cv2.INPAINT_TELEA)
+resize_factor = 0.2
+resize = cv2.resize(inpainted, (0, 0), fx=resize_factor, fy=resize_factor)
+# cv2.imwrite("output.jpg", inpainted)
+cv2.imshow("Watermark Removed", resize)
+cv2.waitKey(0)
